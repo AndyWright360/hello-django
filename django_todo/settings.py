0e7740c312b45ad19d90e8359902be89db0804c0
Andrew Wright <agwright360@gmail.com>
2024-03-20 20:33:02 +0000

    docs: set up development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,9 +1,3 @@
-from pathlib import Path
-import os
-import dj_database_url
-if os.path.isfile('env.py'):
-    import env
-
 """
 Django settings for django_todo project.
 
@@ -17,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*0wk0)5*6cz9d&x4bp2lb6&#$l730@3y5hkj8c)th&8otal=8@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [
-    '8000-andywright3-hellodjango-ns4vcam86kq.ws-eu110.gitpod.io',
-    os.environ.get('HEROKU_HOSTNAME'),
-    ]
+if development:
+    ALLOWED_HOSTS = ['8000-andywright3-hellodjango-ns4vcam86kq.ws-eu110.gitpod.io']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -83,16 +84,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
